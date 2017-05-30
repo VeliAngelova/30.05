3b7012d3866171a901f415498bd549378037ece1
VeliAngelova <vs.angelova@gmail.com>
2017-05-30 20:30:57 +0300

    bbbbbbbbbb

--- C Basic_Syntax_Exercises_23.05/Beverage_Labels/Beverage_Labels.cs
+++ C Basic_Syntax_Exercises_23.05/Beverage_Labels/Beverage_Labels.cs
@@ -4,26 +4,4 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Beverage_Labs
-    {
-        static void Main(string[] args)
-        {
-            var nameN = Console.ReadLine();
-            var volume = int.Parse(Console.ReadLine());
-        
-            var EnergyContent = int.Parse(Console.ReadLine());
-            var SugarContent = int.Parse(Console.ReadLine());
-
-            var volumeNa100 = volume / 100.0;//акo napisha double otpred
-            // a prom sa mi int, 6te gi razdeli kato int i 6te gi zapi6e v double
-            // a taka otnovo se gubi stojnostta sled desetinata zapetaq.
-            // zatova slagame ".0" sled edno ot 4islata.
-            var kcal = volumeNa100 * EnergyContent;
-            var sugarsNaG = volumeNa100 * SugarContent;
-
-            Console.WriteLine("{0}ml" + " {1}:", volume, nameN);
-            Console.WriteLine("{0}kcal," + " {1}g sugars", kcal, sugarsNaG);
-    
-        }}}}}}}}}}}}
-    }
 
